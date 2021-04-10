ffc466fdace841bc5d58c667be51134940c4b8cd
acartaya <alain.cartaya@digital-results-international.com>
2021-04-09 20:13:11 -0400

    Removed unused lines in AdminDashboard

--- client/src/components/Admins/AdminDashboard.js
+++ client/src/components/Admins/AdminDashboard.js
@@ -1,17 +1,11 @@
 import React, {Fragment} from 'react';
-import {Route, Switch, useRouteMatch, withRouter} from 'react-router-dom';
+import {withRouter} from 'react-router-dom';
 import {withStyles} from '@material-ui/core/styles';
 import {connect} from "react-redux";
 import Grid from '@material-ui/core/Grid';
 import Typography from '@material-ui/core/Typography';
 import {blue, blueGrey, grey} from '@material-ui/core/colors';
 import TeamCalendar from "../Teams/TeamCalendar";
-import AdminRoute from "../Routes/AdminRoute";
-import VolunteerManagement from "../../pages/VolunteerManagement";
-import SchoolPersonnelManagement from "../../pages/SchoolPersonnelManagement";
-import SchoolManagement from "../../pages/SchoolManagement";
-import TeamManagement from "../../pages/TeamManagement";
-import AdminManagement from "../../pages/AdminManagement";
 import {Box} from "@material-ui/core";
 
 const useStyles = {
@@ -52,56 +46,43 @@ const useStyles = {
 }
 
 const AdminDashboard = (props) => {
-    let {path} = useRouteMatch();
     const {semesterYear} = props;
 
 
     const getTodaysDate = () => {
-        var options = {weekday: 'long', year: 'numeric', month: 'long', day: 'numeric'};
-        var today = new Date();
+        let options = {weekday: 'long', year: 'numeric', month: 'long', day: 'numeric'};
+        let today = new Date();
         return (today.toLocaleDateString("en-US", options))
     }
 
     return (
         <Fragment>
             <Grid container>
-            <Box
-                style={{textAlign: "center"}}>
-                <Typography
-                    className={props.classes.main}
-                    style={{marginBottom: '15px'}}
-                    align="center"
-                >
-                    Admin Dashboard
-                </Typography>
-            
-
-            <Switch>
-                <Route exact path={path}>
-                    <Grid container direction="column" justify="center" alignItems="center" className={props.classes.custom}>
+                <Box
+                    style={{textAlign: "center"}}>
+                    <Typography
+                        className={props.classes.main}
+                        style={{marginBottom: '15px'}}
+                        align="center"
+                    >
+                        Admin Dashboard
+                    </Typography>
+                    <Grid container direction="column" justify="center" alignItems="center"
+                          className={props.classes.custom}>
 
                         <Typography shadow={3} color="textPrimary" align='center' variant="h6" display="inline"
                                     style={{marginTop: '30px', fontSize: 30, fontWeight: 800, /* color: blue[500] */}}>
                             {semesterYear.semester + " " + semesterYear.year}
                         </Typography>
 
-                        <Typography color="textPrimary" align='center' variant="h6" display="inline" style={{marginTop: '5px'}}>
+                        <Typography color="textPrimary" align='center' variant="h6" display="inline"
+                                    style={{marginTop: '5px'}}>
                             {getTodaysDate()} &nbsp;
                         </Typography>
                     </Grid>
-
                     <TeamCalendar/>
-                </Route>
-                <AdminRoute path={`${path}/volunteer-management`} component={VolunteerManagement}/>
-                <AdminRoute path={`${path}/school-personnel-management`} component={SchoolPersonnelManagement}/>
-                <AdminRoute path={`${path}/school-management`} component={SchoolManagement}/>
-
-                <AdminRoute path={`${path}/team-management`} component={TeamManagement}/>
-
-                <AdminRoute path={`${path}/admin-management`} component={AdminManagement}/>
-            </Switch>
-            </Box>
-          </Grid>
+                </Box>
+            </Grid>
         </Fragment>
     )
 }
