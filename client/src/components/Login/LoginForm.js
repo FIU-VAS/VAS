c3baed557b18cf5aa5c9668b89155c5f820c6253
Michael McCarthy <mmccarthy731@yahoo.com>
2021-04-14 13:59:03 -0400

    Minor fix to error message.

--- client/src/components/Login/LoginForm.js
+++ client/src/components/Login/LoginForm.js
@@ -139,8 +139,9 @@ class LoginForm extends Component {
                 <Alert severity="success">{this.props.success.message}</Alert>
             )
         else if (!isEmpty(this.props.errors.response)) {
+            console.log(this.props.errors.response)
                 return (
-                    <Alert severity="error">{this.props.errors.response.statusCode === 401 ? "Invalid username or password" : this.props.errors.response.statusText}</Alert>
+                    <Alert severity="error">{this.props.errors.response.status === 401 ? "Invalid email or password" : this.props.errors.response.statusText}</Alert>
                 )
         }
     }
